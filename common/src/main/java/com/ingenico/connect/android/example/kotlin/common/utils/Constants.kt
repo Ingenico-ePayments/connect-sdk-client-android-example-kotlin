5623e1aec4715316b755e6050a02587e44d9d10f
Ingenico ePayments <github@epay.ingenico.com>
2022-12-08 14:27:46 +0100

    Release 1.0.2.

--- common/src/main/java/com/ingenico/connect/android/example/kotlin/common/utils/Constants.kt
+++ common/src/main/java/com/ingenico/connect/android/example/kotlin/common/utils/Constants.kt
@@ -7,7 +7,7 @@ package com.ingenico.connect.android.example.kotlin.common.utils
 object Constants {
 
     /** Application Identifier, used for identifying the application in network calls  */
-    const val APPLICATION_IDENTIFIER = "Android Example Application Kotlin/v1.0.1"
+    const val APPLICATION_IDENTIFIER = "Android Example Application Kotlin/v1.0.2"
     const val CARD_NUMBER = "cardNumber"
     const val EXPIRY_DATE = "expiryDate"
     const val SECURITY_NUMBER = "cvv"
